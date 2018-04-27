0c86425791ac4c5251d8add57fba23b2ec53c657
amit04patel <amitpatel.svnit@gmail.com>
2018-04-27 22:02:42 +0400

    prime factors complete

--- Python/Math/factors.py
+++ Python/Math/factors.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
+import math
+
+
 def primeFactors(num):
-    
+    facts = []
+    while num % 2 == 0:
+        facts.append(2)
+        num /= 2
+    for i in range(3, int(math.sqrt(num))+1, 2):
+        while num % i == 0:
+            facts.append(i)
+            num /= i
+    if num>2:
+        facts.append(num)
+    return facts
--- Python/Math/test.py
+++ Python/Math/test.py
@@ -2,6 +2,8 @@
 
 import hcf
 import lcm
+import factors
 
 l = [2, 5, 90]
 print(hcf.findHCF(l))
+print(factors.primeFactors(3))
